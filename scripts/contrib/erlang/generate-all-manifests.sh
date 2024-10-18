78244b35e0ae094e9d1b217bd4665fa843d25762
João Henrique Ferreira de Freitas <joaohf@gmail.com>
2024-10-18 12:15:08 -0300

    erlang: 27.1 -> 27.1.2

--- scripts/contrib/erlang/generate-all-manifests.sh
+++ scripts/contrib/erlang/generate-all-manifests.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-versions=("25.1.2.1" "25.3.2.15" "26.2.5.4" "27.1" "28.0-rc0")
+versions=("25.1.2.1" "25.3.2.15" "26.2.5.4" "27.1.2" "28.0-rc0")
 
 for version in ${versions[@]}
 do
