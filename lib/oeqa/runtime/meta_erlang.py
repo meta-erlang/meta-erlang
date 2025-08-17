b8259299bda02ddce0ab6d4b40423d6208b192c0
João Henrique Ferreira de Freitas <joaohf@gmail.com>
2025-08-16 21:04:27 -0300

    oeqa/runtime: Introduce Lux tests with testimage
    Lux [1] is a test framework and this commit introduces
    the support for running lux script when testing images
    with oeqa framework [2].
    
    1: Lux (LUcid eXpect scripting) is a test automation
       framework with Expect style execution of commands
       https://github.com/hawk/lux
    
    2: https://docs.yoctoproject.org/test-manual/runtime-testing.html

---
+++ lib/oeqa/runtime/meta_erlang.py
@@ -0,0 +1,107 @@
+import os
+
+from shutil import rmtree
+from oeqa.runtime.case import OERuntimeTestCase
+
+class MetaErlangTestCase(OERuntimeTestCase):
+    """
+    Prepare and run Lux test cases on target image.
+
+    This class copy all local files from 'files/lux/support'
+    to LUX_WORKDIR on target before.
+
+    The decorator LuxTestCase is responsible for configuring the
+    lux test case to be executed and also where the lux tests will
+    be copied from.
+
+    Methods
+    -------
+    run_lux_test_case()
+        run a lux test case
+    """
+
+    LUX_WORKDIR = '/tmp/lux'
+    LUX_SUPPORT_DIR = 'lux/support'
+    LUX_TIMEOUT = 1200
+
+
+    @classmethod
+    def setUpClass(cls):
+        cls.lux_startup()
+
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.lux_finishup()
+
+
+    @classmethod
+    def lux_startup(cls):
+        cls._lux_copy_support_files()
+
+        if not hasattr(cls.tc, "extraresults"):
+            cls.tc.extraresults = {}
+        cls.extras = cls.tc.extraresults
+        cls.extras['lux.rawlogs'] = {'log': ""}
+
+
+    @classmethod
+    def lux_finishup(cls):
+        cls.tc.target.run(f'rm -rf {MetaErlangTestCase.LUX_WORKDIR}')
+
+
+    @classmethod
+    def _lux_copy_support_files(cls):
+        myfilesdir = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'files')
+        src = os.path.join(myfilesdir, MetaErlangTestCase.LUX_SUPPORT_DIR)
+        _, _ = cls.tc.target.run(f'mkdir -p {MetaErlangTestCase.LUX_WORKDIR}')
+        cls.tc.target.copyTo(src, MetaErlangTestCase.LUX_WORKDIR)
+
+
+    def run_lux_test_case(self):
+        """
+        Run a lux test case previously configured using LuxTestCase decorator.
+
+        After finishing test execution, all logs will be transferred to
+        'WORKDIR/lux_logs' folder.
+        """
+        luxscript = self.tc._registry['lux']['test_case']
+        test_case_dir = self.tc._registry['lux']['test_case_dir']
+        workdir = os.path.join(MetaErlangTestCase.LUX_WORKDIR, test_case_dir)
+
+        log_dir = self._get_lux_log_dir(luxscript)
+
+        lux_command = f'cd {workdir}; lux --log_dir {log_dir} {luxscript}'
+
+        status, output = self.target.run(lux_command, timeout=MetaErlangTestCase.LUX_TIMEOUT)
+
+        self.extras['lux.rawlogs']['log'] = self.extras['lux.rawlogs']['log'] + output
+
+        self._transfer_logs(luxscript)
+
+        msg = f'Test case {luxscript} failed, workdir {workdir} \
+              lux command: {lux_command} output: {output}'
+        self.assertEqual(status, 0, msg=msg)
+
+        return (status, output)
+
+
+    # Copy the log files to be parsed locally
+    def _transfer_logs(self, luxscript):
+        local_logs = os.path.join(self.td.get('WORKDIR'), 'lux_logs')
+        if os.path.exists(local_logs):
+            rmtree(local_logs)
+        os.makedirs(local_logs)
+        log_dir = self._get_lux_log_dir(luxscript)
+        self.target.copyFrom(log_dir, local_logs)
+
+
+    def _get_testcase(self, luxscript):
+        testcase, _ = os.path.splitext(luxscript)
+        return testcase
+
+
+    def _get_lux_log_dir(self, luxscript):
+        return os.path.join(MetaErlangTestCase.LUX_WORKDIR,
+                            'logs',
+                            self._get_testcase(luxscript))
