c94572e2a0d90cb51211a3a083305e3046ecd76d
João Henrique Ferreira de Freitas <joaohf@gmail.com>
2026-03-23 09:41:56 -0300

    erlang: bump versions
     26.2.5.17 -> 26.2.5.18
     27.3.4.8 -> 27.3.4.9
     28.4 -> 28.4.1
     29.0-rc1 -> 29.0-rc2

--- scripts/contrib/erlang/generate-all-manifests.sh
+++ scripts/contrib/erlang/generate-all-manifests.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-versions=("25.3.2.21" "26.2.5.17" "27.3.4.8" "28.4" "29.0-rc1")
+versions=("25.3.2.21" "26.2.5.18" "27.3.4.9" "28.4.1" "29.0-rc2")
 
 for version in ${versions[@]}
 do
